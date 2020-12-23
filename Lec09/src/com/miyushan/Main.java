839a563fb39aa6e62461c8629ee969c1524e5431
Miyushan <smiushan2015@gmail.com>
2020-12-23 18:29:44 +0530

    packages, classes and methods

--- Lec09/src/com/miyushan/Main.java
+++ Lec09/src/com/miyushan/Main.java
@@ -3,7 +3,6 @@ package com.miyushan;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello!!!");
+        Miyushan.shakeer();
     }
 }
---
+++ Lec09/src/com/miyushan/Miyushan.java
@@ -0,0 +1,12 @@
+package com.miyushan;
+
+public class Miyushan {
+
+    public static void main(String[] args) {
+        System.out.println("Hii from Miyushan");
+        shakeer();
+    }
+    public static void shakeer(){
+        System.out.println("Hii from Shakeer");
+    }
+}
