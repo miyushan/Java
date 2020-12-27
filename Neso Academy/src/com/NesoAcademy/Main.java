1c7f35173cf34ed7cf86b14f97e056a83b8a4aca
Miyushan <smiushan2015@gmail.com>
2020-12-27 14:20:28 +0530

    Constants in Java Programming

---
+++ Neso Academy/src/com/NesoAcademy/Main.java
@@ -0,0 +1,11 @@
+package com.NesoAcademy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final String COMPANY_NAME = "Neso Accademy";
+        //can't assign a new value to a constant
+//        COMPANY_NAME = "Another company";
+        System.out.println(COMPANY_NAME);
+    }
+}
