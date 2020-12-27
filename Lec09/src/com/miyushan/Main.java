26188b1e16e789112450ac5e96bcea37b787e636
Miyushan <smiushan2015@gmail.com>
2020-12-27 13:50:00 +0530

    Practice variables

--- Lec09/src/com/miyushan/Main.java
+++ Lec09/src/com/miyushan/Main.java
@@ -1,11 +1,23 @@
 package com.miyushan;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
 
-        Miyushan.sayName();
-        Miyushan.sayAge();
+        String myName = "Miyushan";
+        String myJob = "Programmer";
+        String myNameCopy = myName;
+        myName = "Shakeer";
 
+        System.out.println(myName);
+        System.out.println(myJob);
+        System.out.println(myNameCopy);
+        System.out.println(myName);
+
+        //call by configurations
+//        String arg1 = args[0];
+//        System.out.println(arg1);
+//        System.out.println(args[1]);
     }
+
 }
