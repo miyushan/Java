16d9bf9f4d3179bf3571b065df9fbf4c35874ce1
Miyushan <smiushan2015@gmail.com>
2021-01-02 23:38:13 +0530

    calling String methods in Java

--- Lec28/src/com/LearnJava/Main.java
+++ Lec28/src/com/LearnJava/Main.java
@@ -4,16 +4,47 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //String data type is a class
-        
-        //convert a normal text into uppercase and print it
-        String text = "Welcome Java";
-        System.out.println(text.toUpperCase());
+        String s1 = "NeSo AcaDEMy";
 
-        //convert a normal text into uppercase by using a new variable
-        String text2 = "Welcome Java";
-        String upperCaseText = text2.toUpperCase();
-        System.out.println(upperCaseText);
+        // toUpperCase method
+        System.out.println(s1.toUpperCase());
+
+        // toLowerCase method
+        System.out.println(s1.toLowerCase());
+
+        //original text
+        System.out.println(s1);
+
+        //length method
+        int i = s1.length();
+        System.out.println("length: " + i);
+
+        //isEmpty method
+        boolean b = s1.isEmpty();
+        System.out.println("isEmpty: " + b);
+
+        //isBlank method
+        String s2 = "   ";
+        boolean b1 = s2.isBlank();
+        System.out.println("isBlank: " + b1);
+
+        //charAt method
+        char c = s1.charAt(3);     //4th character
+        System.out.println("charAt: " + c);
+
+
+        String s3 = "saghhas";
+        //indexOf method
+        int i2 = s3.indexOf('a');
+        System.out.println("indexOf: " + i2);       //1
+
+        //lastIndexOf method
+        int i3 = s3.lastIndexOf('a');
+        System.out.println("lastIndexOf: " + i3);       //5
+
+        //concat method
+        String s4 = s1.concat(" lessons");
+        System.out.println("concat: " + s4);
 
     }
 }
