5475c6ef6499411f7c0ba13f9ff144e006ca8ac3
Miyushan <smiushan2015@gmail.com>
2021-01-02 23:56:04 +0530

    Concatenating Strings in Java

--- Lec28/src/com/LearnJava/Main.java
+++ Lec28/src/com/LearnJava/Main.java
@@ -4,47 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String s1 = "NeSo AcaDEMy";
+        //concatenation
+        System.out.println("neso" + "academy");
+        System.out.println("neso" + " " + "academy");
+        System.out.println("neso " + "academy");
+        System.out.println("neso" + 5);
+        System.out.println("neso" + 5 + 3);     //won't add 5 and 3 (Because all elements act as strings)
+        System.out.println("neso" + (5 + 3));   //add 5 and 3 (5+3=8)
 
-        // toUpperCase method
-        System.out.println(s1.toUpperCase());
-
-        // toLowerCase method
-        System.out.println(s1.toLowerCase());
-
-        //original text
-        System.out.println(s1);
-
-        //length method
-        int i = s1.length();
-        System.out.println("length: " + i);
-
-        //isEmpty method
-        boolean b = s1.isEmpty();
-        System.out.println("isEmpty: " + b);
-
-        //isBlank method
-        String s2 = "   ";
-        boolean b1 = s2.isBlank();
-        System.out.println("isBlank: " + b1);
-
-        //charAt method
-        char c = s1.charAt(3);     //4th character
-        System.out.println("charAt: " + c);
-
-
-        String s3 = "saghhas";
-        //indexOf method
-        int i2 = s3.indexOf('a');
-        System.out.println("indexOf: " + i2);       //1
-
-        //lastIndexOf method
-        int i3 = s3.lastIndexOf('a');
-        System.out.println("lastIndexOf: " + i3);       //5
-
-        //concat method
-        String s4 = s1.concat(" lessons");
-        System.out.println("concat: " + s4);
-
+        System.out.println("");
+        System.out.println("Neso".concat(" Academy").concat("5"));  //any String can call its methods by a Dot operator
     }
 }
