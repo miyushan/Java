74fc013bf00c94a15f40cbc4981013aae7c12da0
Miyushan <smiushan2015@gmail.com>
2021-01-02 20:30:36 +0530

    The char Data type in Java

--- Lec28/src/com/LearnJava/Main.java
+++ Lec28/src/com/LearnJava/Main.java
@@ -3,16 +3,16 @@ package com.LearnJava;
 public class Main {
 
     public static void main(String[] args) {
-        float f1 = 4.5f;
-        //float f2 = 4.5;   can't
-        double d1 = 4.5;
-        double d2 = 4.5f;   //float converts to double type
-        //int i1 = 4.5;     can't
-        double d3 = 4;
-        System.out.println("f1: " + f1);
-        System.out.println("d1: " + d1);
-        System.out.println("d2: " + d2);
-        System.out.println("d3: " + d3);
-        System.out.println("d2+d3: " + (d2+d3));
+        char c1 = 'A';      //A
+        char c2 = 65;       //A     //as an integer value
+        char c3 = '\u0041'; //A     //as an unicode
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+        int i1 = 'A';       //65;
+        int i2 = '\u0041';  //65
+        System.out.println(i1);
+        System.out.println(i2);
     }
 }
